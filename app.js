4c41b133dba66910ed89ed188487926405056569
nishit <nishitranjan41@gmail.com>
2025-01-26 00:00:51 +0530

    lesson-1

---
+++ app.js
@@ -0,0 +1,10 @@
+require("./xyz");
+const { sum } = require("./sum")
+const {multiply} = require("./multiply")
+a = 5;
+b=10;
+console.log("sum =>",sum(a,b));
+console.log("multiply=>",multiply(a,b));
+
+
+
